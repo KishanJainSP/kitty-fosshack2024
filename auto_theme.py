f0c62a860d58783d431e0c4d342ed02cada85997
KishanJainSP <kjainsp000@gmail.com>
2024-07-23 14:18:40 +0530

    Create auto_theme.py
    program that Checks the Color Scheme of a Operating System and sets light theme or Dark theme in Kitty accordingly

---
+++ auto_theme.py
@@ -0,0 +1,45 @@
+import os
+import json
+import ipc
+import platform
+import subprocess
+
+def get_system_color_scheme():
+    os_name = platform.system()
+    if os_name in ['FreeBSD', 'OpenBSD', 'NetBSD', 'DragonFly']:
+        output = subprocess.check_output(['sysctl', '-n', 'hw.cfb.mode'])
+        if (output=='3'or'2'):
+            scheme='Dark'
+    pass
+    elif os_name == 'Darwin':
+        output = subprocess.check_output(['defaults', 'read', 'NSGlobalDomain', 'AppleAquaGraphite'])
+        if(output=='1'):
+            scheme='Dark'
+    pass
+    elif os_name == 'Linux':
+        output = subprocess.check_output(['gsettings', 'get', 'org.gnome.desktop.interface', 'gtk-theme'])
+        if ('dark'in output):
+            scheme='Dark'
+    pass
+
+
+
+light_theme, dark_theme = kitty.conf.get('light_and_dark_mode_themes', '').split(',')
+mutex = ipc.Mutex('kitty_theme_mutex')
+
+def on_system_color_scheme_change(scheme):
+    get_system_color_scheme()
+    if scheme == 'Dark':
+        theme = dark_theme
+    else:
+        theme = light_theme
+    with mutex:
+        try:
+            output = kitten('themes', '--dump-theme', '--check-age=-1', theme)
+            colors = json.loads(output)
+        except Exception:
+            output = kitten('themes', '--dump-theme', '--cache-age=0', theme)
+            colors = json.loads(output)
+    Boss.patch_colors(colors)
+
+Boss.on_system_color_scheme_change = on_system_color_scheme_change
